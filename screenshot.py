7bd43d77010f5de784d6712f127877d1fd133126
Oscar Rosario <oscar.inirio@gmail.com>
2014-05-23 14:49:26 -0400

    added date and time support

--- screenshot.py
+++ screenshot.py
@@ -1,1 +1,12 @@
 from PIL import ImageGrab
+import datetime
+import time
+
+# ask for delay between screenshots
+print 'How many seconds do you want between screenshots?'
+seconds = int(input())
+
+# get date and time
+currentTime = time.time()
+currentDate = datetime.datetime.fromtimestamp(currentTime).strftime('%Y-%m-%d %H-%M-%S')
+
