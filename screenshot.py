9d9819d1680e7f67ccbb06ccbcae852bd7936c05
Oscar Rosario <indexmarket@billioninternational.com>
2014-05-23 15:17:39 -0400

    added working directory and bug fixes

--- screenshot.py
+++ screenshot.py
@@ -15,6 +15,14 @@ if fileExtension == 1:
 else:
     extension = '.jpg'
 
+directory = os.getenv('APPDATA') + '//screencaptures'
+
+try:
+    os.chdir(directory)
+except:
+    os.mkdir(directory)
+    os.chdir(directory)
+
 
 # get date and time
 currentTime = time.time()
@@ -25,14 +33,12 @@ def screen_grab():
     fileName = str(currentDate) + ".png"
     im.save(fileName)
 
-def find_oldest_file(dirname,extension):
+def find_oldest_file(dirname,ext):
     oldest_file, oldest_time = None, None
     for dirpath, dirs, files in os.walk(dirname):
         for filename in files:
             file_path = os.path.join(dirpath, filename)
-            file_time os.stat(file_path.st_mtime
+            file_time = os.stat(file_path).st_mtime
             if file_path.endswith(extension) and (file_time<oldest_time or oldest_time is None):
                 oldest_file, oldest_time = file_path, file_time
     return oldest_file
-
-
